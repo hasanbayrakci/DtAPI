0e3a72f0e7da9881c2654ead0a7396bd505856d9
Administrator <Administrator@WIN-88CSJMMUTPK>
2021-09-14 12:04:08 +0300

    Add project files.

---
+++ Controllers/DtApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DtAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DtApiController : ControllerBase
+    {
+
+        [HttpGet]
+        public DtApi Get()
+        {
+            return new DtApi
+            {
+                
+            };
+        }
+    }
+}
---
+++ DtApi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DtAPI
+{
+    public class DtApi
+    {
+        public int user_id { get; set; }
+        public string kurum_adi { get; set; }
+        public string ilan_adi { get; set; }
+        public string unique_id { get; set; }
+        public string ilan_data { get; set; }
+        public int durum { get; set; }
+
+    }
+}
